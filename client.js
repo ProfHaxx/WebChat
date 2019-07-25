c06d1f9395832819e5f159757c3faaa5c7e2af10
Blanc L. Leedom <profcode@localhost.localdomain>
2019-07-25 07:47:24 +0200

    v1.3

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+var username = "Anonymous";
+
 var socket = io.connect();
 socket.on('connect', function (data) {
     console.log(data);
@@ -11,14 +13,14 @@ socket.on('broad', function (data) {
 $('form').submit(function (e) {
     e.preventDefault();
     var message = $('#chat_input').val();
-    socket.emit('messages', message);
+    socket.emit('messages', "[" + username + "]: " + message);
 });
 
 function navigate(loc) {
     switch(loc) {
         case undefined:
             document.getElementById("header").innerHTML = "Chat";
-            document.getElementById("future").style.display = "inline";
+            document.getElementById("future").style.display = "block";
             document.getElementById("form").style.display = "inline";
             document.getElementById("theme_select").style.display = "none";
             break;
@@ -38,6 +40,35 @@ function navigate(loc) {
 }
 
 function selectTheme() {
-    document.getElementById("body").style.color = document.getElementById("bg_box").value;
-    document.getElementById("body").style.color = document.getElementById("font_box").value;
+    var body = document.getElementById("body").style;
+    var dark = document.getElementById("dark_elem").style;
+    var light = document.getElementById("light_elem").style;
+
+    var font = document.getElementById("font_box").value;
+    var bg = document.getElementById("bg_box").value;
+    var nav_D = document.getElementById("navd_box").value;
+    var nav_L = document.getElementById("navl_box").value;
+
+    if(bg != "") {
+        body.backgroundImage = "linear-gradient(to right, rgba(" + bg + "), rgba(" + bg + "))";
+    }
+
+    if(font != "") {
+        body.color = font;
+    }
+
+    if(nav_D != "") {
+        dark.background = nav_D;
+    }
+
+    if (nav_L != "" && nav_D != "") {
+        var css = "#lightelem { background-color:" + nav_D + " } #lightelem:hover{ background-color: " + nav_L + " }";
+        var style = document.createElement('style');
+
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+    }
 }
